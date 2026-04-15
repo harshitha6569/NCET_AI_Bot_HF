8477332e9c385d42bef4accf87d54d974053fdd5
harshitha6569 <harshitha6569@gmail.com>
2026-04-15 15:39:10 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,24 +1,29 @@
 import streamlit as st
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+
 @st.cache_resource
 def load_summarizer():
-  return pipeline("Summarization",model="sshleifer/distibart-cnn-12-6")
-summarizer=load_summrizer()
-st.title("AI Text Summarizer")
-st.write("Enter a long text below, and get a concise Summary!")
-long_text = st.text_area("Enter text to Summarizer:", height = 200)
-max_lenght = st.slider("Max Summary Length",min_value=50,
-                       max_value=300,value=130)
-min_lenght = st.slider("Min Summary Length",min_value=20,
-                       max_value=100,value=30)
-with st.button("Summarize"):
-  if long_text.strip():
-    with st.spinner("Generating summary..."):
-      summary = summarizer(long_text, max_length=max_length,
-                           min_length=min_length,do_sample=False)
-      st.subheader("Summary:")
-      st.success(summary[0]['summary_text'])
-  else:
-    st.warning("Please enter some text to summarize")
+    model_name = "sshleifer/distilbart-cnn-12-6"
     
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    
+    return pipeline(
+        task="summarization",   
+        model=model,
+        tokenizer=tokenizer
+    )
+
+summarizer = load_summarizer()
+
+st.title("AI Text Summarizer")
+
+text = st.text_area("Enter text:")
+
+if st.button("Summarize"):
+    if text.strip():
+        result = summarizer(text, max_length=130, min_length=30)
+        st.write(result[0]["summary_text"])
+    else:
+        st.warning("Please enter some text to summarize.")
 
